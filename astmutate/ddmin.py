8bef7b4e1be7b8f40357eb3410d2b5b8baef1db6
Rahul Gopinath <rahul@gopinath.org>
2018-01-05 11:28:30 -0800

    udpate

--- astmutate/ddmin.py
+++ astmutate/ddmin.py
@@ -1,28 +1,12 @@
 import random
 import string
 
-# our use of ddmin.
 # if the mutant fails in one of the test cases, we are happy 
 #           (TODO: Threats -- we are not ensuring same test case fails.)
 # we split the mutations in the mutants into two, and evaluate both.
 # if at least one succeeds, then it is great, we descent into that.
 # if both succeeds, then we have to descent into both.
 # if none succeeds, then we do the ddmin thing and increase the coarseness
-
-def complement(s, i, l): return s[:i] + s[i + l:]
-
-def all_complements_failing(s, npartitions, testfn):
-    subset_length = len(s) // npartitions
-    items = range(0,len(s)-1, subset_length)
-    complements = [complement(s, i, subset_length) for i in items]
-    return [i for i in complements if testfn(i)] # return all complements, not just first
-
-def update_input(s, npartitions, fn):
-    v = all_complements_failing(s, npartitions, fn)
-    if v:
-        return v, max(npartitions - 1, 2)
-    else:
-        return [s], min(npartitions * 2, len(s))
 
 def minimize(s, fn):
     # split the input into two, and check each.
@@ -54,22 +38,4 @@ def minimize(s, fn):
         # also unlikely to have failing faults.
         return []
     else: assert False
-
-
-
-    npartitions = 2
-    lst = [s]
-    collected = []
-    while lst:
-        s, *lst = lst
-        newlst, n1 = update_input(s, npartitions, fn)
-        # npartitions is the number of partitions. We stop when the number of partitions
-        # equal the number of individual elements.
-        if len(newlst) == 1 && npartitions == len(newlst[0]):
-            collected.append(newlst[0])
-            continue
-        npartitions = n1
-        lst.extend(newlst)
-    return s
-
 
